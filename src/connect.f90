8c83ce1b44b9fc13fea53a75ba9bd88e673d6e37
Sean <r04246004@ntu.edu.tw>
2018-12-27 04:25:46 -0600

    modify format

--- src/connect.f90
+++ src/connect.f90
@@ -1,6 +1,9 @@
       subroutine rdparam(UnitNum)
+
       use INPUT
+
       implicit none
+
       integer :: I, ERR, UnitNum 
 
 !      OPEN (UNIT=9,FILE='pspm2d.rea',STATUS='OLD',iostat=ierr)
@@ -8,6 +11,7 @@
       READ(UnitNum,*) 
       READ(UnitNum,*) 
       READ(UnitNum,*) NPARAM
+
       DO 20 I=1,NPARAM
          READ(UnitNum,*) PARAM(I)
 !         write(*,*)PARAM(I)
